47fa788481f9146da87aa88c27aa17b0a4e21a59
Stevan Del Arisandi <42829285+delarisandi@users.noreply.github.com>
2020-02-17 21:06:27 +0700

    Add files via upload

---
+++ 2015/form_admin.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+&lt;html&gt; <br>
+&lt;head&gt;&lt;title&gt;Form Login Admin&lt;/title&gt;&lt;/head&gt; <br>
+&lt;body&gt; <br>
+&lt;form action="login.php" method="post"&gt; <br>
+&lt;table&gt; <br>
+&lt;tr&gt; <br>
+&lt;td&gt;Username&lt;/td&gt; <br>
+&lt;td&gt;&lt;input type="text" name="username" size="20"&gt;&lt;/td&gt; <br>
+&lt;/tr&gt; <br>
+&lt;tr&gt; <br>
+&lt;td&gt;Password&lt;/td&gt; <br>
+&lt;td&gt;&lt;input type="password" name="password" size="20"&gt;&lt;/td&gt; <br>
+&lt;/tr&gt; <br>
+&lt;tr&gt; <br>
+&lt;td&gt;&amp;nbsp;&lt;/td&gt; <br>
+&lt;td&gt;&lt;input type="submit" name="Login" value="Proses"&gt;&lt;/td&gt; <br>
+&lt;/tr&gt; <br>
+&lt;/table&gt; <br>
+&lt;/form&gt; <br>
+&lt;/body&gt; <br>
+&lt;/html&gt;
+</body>
+</html>
